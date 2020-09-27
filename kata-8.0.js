1f99077e6eceeaf40b381ca6ee16913abda68173
Hanna Oh Andrews <hannaohandrews@gmail.com>
2020-09-27 15:16:18 -0700

    adding kata 8

--- kata-8.0.js
+++ kata-8.0.js
@@ -1,5 +1,15 @@
-const multiplicationTable = function(maxValue) {
-  // Your code here
+const multiplicationTable = function (maxValue) {
+  let newArray = [];
+  for (let x = 1; x < maxValue + 1; x++) {
+    for (let y = 1; y < maxValue + 1; y++) {
+      if (y === maxValue) {
+        newArray += x * y + "\n";
+      } else {
+        newArray += x * y + " ";
+      }
+    }
+  }
+  return newArray;
 };
 
 console.log(multiplicationTable(1));
