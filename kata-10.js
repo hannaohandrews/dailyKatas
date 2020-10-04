7cc34ab64ea644579c133a79cb5e630fe4e212e8
Hanna Oh Andrews <hannaohandrews@gmail.com>
2020-10-04 12:00:11 -0700

    kata10

--- kata-10.js
+++ kata-10.js
@@ -1,61 +1,57 @@
-const chooseRecipe = function(bakeryA, bakeryB, recipes) {
+const chooseRecipe = function (bakeryA, bakeryB, recipes) {
+  let ingredients = [...bakeryA, ...bakeryB];
+  let validRecipe = '';
 
-let ingredients = [];
+  recipes.forEach(recipe => {
+      for (let j = 0 ; j < recipe.ingredients.length; j++){
+          let firstIngredient = recipe.ingredients[j]
+          let secondIngredient = recipe.ingredients[j+1]
 
-   let ingredientCheck = (bakery,recipe) => {
-    for (let i = 0 ; i < bakery.length; i++){
-        for (let j = 0 ; j < recipe[i][ingredients].length ; j ++){
-            if (bakery[i] === recipe[i][ingredients][j]){
-                ingredients.push(bakery[i])
-            }
-        }
-       }
-       return ingredients
-   }
-   console.log(ingredientCheck(bakeryA,recipes)) 
-   console.log(ingredientCheck(bakeryB,recipes))
-
-  }
-  
-  let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
-  let bakeryB = ['milk', 'butter', 'cream cheese'];
-  let recipes = [
-      {
-          name: 'Coconut Sponge Cake',
-          ingredients: ['coconut', 'cake base']
-      },
-      {
-          name: 'Persian Cheesecake',
-          ingredients: ['saffron', 'cream cheese']
-      },
-      {
-          name: 'Custard Surprise',
-          ingredients: ['custard', 'ground beef']
+         if (ingredients.includes(firstIngredient) && ingredients.includes(secondIngredient)){
+             validRecipe = recipe.name
+         }
       }
-  ];
+  });
+  return validRecipe
+};
 
-  console.log(chooseRecipe(bakeryA, bakeryB, recipes));
-  
-  bakeryA = ['potatoes', 'bay leaf', 'raisins'];
-  bakeryB = ['red bean', 'dijon mustard', 'apples'];
-  recipes = [
-      {
-          name: 'Potato Ganache',
-          ingredients: ['potatoes', 'chocolate']
-      },
-      {
-          name: 'Sweet Fish',
-          ingredients: ['anchovies', 'honey']
-      },
-      {
-          name: "Nima's Famous Dijon Raisins",
-          ingredients: ['dijon mustard', 'raisins']
-      }
-  ];
-  
-  console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+// let bakeryA = ["saffron", "eggs", "tomato paste", "coconut", "custard"];
+// let bakeryB = ["milk", "butter", "cream cheese"];
+// let recipes = [
+//   {
+//     name: "Coconut Sponge Cake",
+//     ingredients: ["coconut", "cake base"],
+//   },
+//   {
+//     name: "Persian Cheesecake",
+//     ingredients: ["saffron", "cream cheese"],
+//   },
+//   {
+//     name: "Custard Surprise",
+//     ingredients: ["custard", "ground beef"],
+//   },
+// ];
 
-  // Persian Cheesecake 
-  // Nima's Famous Dijon Raisins 
+// console.log(chooseRecipe(bakeryA, bakeryB, recipes));
 
+bakeryA = ["potatoes", "bay leaf", "raisins"];
+bakeryB = ["red bean", "dijon mustard", "apples"];
+recipes = [
+  {
+    name: "Potato Ganache",
+    ingredients: ["potatoes", "chocolate"],
+  },
+  {
+    name: "Sweet Fish",
+    ingredients: ["anchovies", "honey"],
+  },
+  {
+    name: "Nima's Famous Dijon Raisins",
+    ingredients: ["dijon mustard", "raisins"],
+  },
+];
 
+console.log(chooseRecipe(bakeryA, bakeryB, recipes));
+
+// Persian Cheesecake
+// Nima's Famous Dijon Raisins
