7f38374a517254e7d6489361f3d9f1a5e45b8ce9
Hanna Oh Andrews <hannaohandrews@gmail.com>
2020-10-04 12:28:42 -0700

    talking calender

--- TalkingCalender.js
+++ TalkingCalender.js
@@ -1,7 +1,20 @@
 const talkingCalendar = function(date) {
-    // Your code here
+
+ let array = date.split('/')
+    // console.log(array)
+    // console.log(array[1])
+let months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+
+for (let i = 0; i < 13; i++){
+    
+}
+
   };
   
   console.log(talkingCalendar("2017/12/02"));
   console.log(talkingCalendar("2007/11/11"));
   console.log(talkingCalendar("1987/08/24"));
+
+// December 2nd, 2017
+// November 11th, 2007
+// August 24th, 1987
