fa8fff92095b2d19b21531696df5b00686d22272
Hanna Oh Andrews <hannaohandrews@gmail.com>
2020-10-04 16:25:00 -0700

    added months and days

--- TalkingCalender.js
+++ TalkingCalender.js
@@ -1,19 +1,43 @@
-const talkingCalendar = function(date) {
+const talkingCalendar = function (date) {
+  let array = date.split("/");
+  let monthNum = array[1];
+  let day = array[2];
+  let year = array[0];
 
- let array = date.split('/')
-    // console.log(array)
-    // console.log(array[1])
-let months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+  let answer = "";
 
-for (let i = 0; i < 13; i++){
-    
-}
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = ["1st", "2nd", "3rd"];
 
-  };
-  
-  console.log(talkingCalendar("2017/12/02"));
-  console.log(talkingCalendar("2007/11/11"));
-  console.log(talkingCalendar("1987/08/24"));
+  if (day === "01") {
+    answer = months[Number(monthNum) - 1] + " " + days[0] + ", " + year;
+  } else if (day === "02") {
+    answer = months[Number(monthNum) - 1] + " " + days[1] + ", " + year;
+  } else if (day === "03") {
+    answer = months[Number(monthNum) - 1] + " " + days[2] + ", " + year;
+  } else {
+    answer = months[Number(monthNum) - 1] + " " + day + "th" + ", " + year;
+  }
+  return answer;
+};
+console.log(talkingCalendar("2017/12/01"));
+console.log(talkingCalendar("2017/12/02"));
+console.log(talkingCalendar("2017/12/03"));
+console.log(talkingCalendar("2007/11/11"));
+console.log(talkingCalendar("1987/08/24"));
 
 // December 2nd, 2017
 // November 11th, 2007
