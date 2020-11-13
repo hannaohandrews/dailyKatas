2fcb58d28965d869b7aafbc160e5ffca2a6c6ab3
Hanna Oh <63070798+hannaohandrews@users.noreply.github.com>
2020-11-13 09:15:29 -0800

    exercise 1

---
+++ udemy/arrays/Exercise1.js
@@ -0,0 +1,14 @@
+const numbers = arrayFromRange(-10, -4);
+
+console.log(numbers);
+
+function arrayFromRange(min, max) {
+  const array = [];
+  for (let i = -10; i <= -4; i++) {
+    array.push(i);
+  }
+  return array;
+}
+
+//ANS//
+// [-10,-9,-8,-7,-6,-5,-4]
