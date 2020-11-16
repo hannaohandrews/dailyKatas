1fc4552be6238bf8212034d8b7dfa99aab3e9a13
Hanna Oh <63070798+hannaohandrews@users.noreply.github.com>
2020-11-16 09:35:16 -0800

    except

--- udemy/arrays/exercise2.js
+++ udemy/arrays/exercise2.js
@@ -1,14 +1,12 @@
 const numbers = [1, 2, 3, 4];
 
-console.log(numbers.includes(1));
-console.log(numbers.includes(5))
-
 function includes(array, searchElement) {
- return array.map((item) => {
-      if (item === searchElement){
-          return true;
-      } else {
-          return false;
-      }
-  });
+  for (let number of array) {
+    if (number === searchElement) 
+      return true;
+    console.log(number)
+  }  return false;
 }
+
+console.log(includes(numbers, 1));
+console.log(includes(numbers, 6));
---
+++ udemy/arrays/exercise3.js
@@ -0,0 +1,29 @@
+const numbers = [1, 2, 3, 4, 11.1, 2, 1];
+
+const output = except(numbers, [1, 2]);
+
+console.log(output);
+
+function except(array, excluded) {
+  let newArray = [];
+
+  //   for(let item of array){
+  //       if(item != excluded){
+  //           newArray.push(item)
+  //       }
+  //   }
+  //   return newArray;
+
+  //   array.map((item) => {
+  //     if (item != excluded) {
+  //       newArray.push(item);
+  //     }
+  //   });
+
+  for (let element of array)
+    if (!excluded.includes(element)) {
+      newArray.push(element);
+    }
+
+  return newArray;
+}
